fa4d7d6aa83c53a575efd1bd662ca8b5e8fa0c59
yash verma <63188505+yash3000@users.noreply.github.com>
2023-06-23 14:09:14 +0530

    Delete src directory

--- src/components/Button/Button.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import "./Button.css";
-const Button = ({ text }) => <button className="btn">{text}</button>;
-
-export default Button;
--- src/components/Logo/Logo.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import logo from "../../assets/logoImage.png";
-import "./Logo.css";
-
-const Logo = () => <img className="logo-image" src={logo} alt="logo" />;
-
-export default Logo;
